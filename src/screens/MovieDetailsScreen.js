11b787b4c7a09acb26334d88af8dcc0028851081
Muvaffak Onus <onus.muvaffak@gmail.com>
2018-04-26 16:50:52 +0300

    Connection with backend is completed.
    - Given titleId, fill up the boxes.
    
    jira SPRCOUR8-22

--- src/screens/MovieDetailsScreen.js
+++ src/screens/MovieDetailsScreen.js
@@ -1,91 +1,11 @@
 import React from 'react';
 import { View, Image, Text, Button, Subtitle, ListView } from '@shoutem/ui';
 import { connect } from 'react-redux';
+import axios from 'axios';
 
 import { usernameChanged } from '../actions';
 
 const image_path = 'http://image.tmdb.org/t/p/original'
-const testMovieJson = {
-  "adult": false,
-  "backdrop_path": "/s2bT29y0ngXxxu2IA8AOzzXTRhd.jpg",
-  "belongs_to_collection": null,
-  "budget": 160000000,
-  "genres": [
-    {
-      "id": 28,
-      "name": "Action"
-    },
-    {
-      "id": 53,
-      "name": "Thriller"
-    },
-    {
-      "id": 878,
-      "name": "Science Fiction"
-    },
-    {
-      "id": 9648,
-      "name": "Mystery"
-    },
-    {
-      "id": 12,
-      "name": "Adventure"
-    }
-  ],
-  "homepage": "http://inceptionmovie.warnerbros.com/",
-  "id": 27205,
-  "imdb_id": "tt1375666",
-  "original_language": "en",
-  "original_title": "Inception",
-  "overview": "Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life as payment for a task considered to be impossible: \"inception\", the implantation of another person's idea into a target's subconscious.",
-  "popularity": 44.729096,
-  "poster_path": "/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
-  "production_companies": [
-    {
-      "id": 923,
-      "logo_path": "/5UQsZrfbfG2dYJbx8DxfoTr2Bvu.png",
-      "name": "Legendary Pictures",
-      "origin_country": "US"
-    },
-    {
-      "id": 9996,
-      "logo_path": "/3tvBqYsBhxWeHlu62SIJ1el93O7.png",
-      "name": "Syncopy",
-      "origin_country": "GB"
-    },
-    {
-      "id": 174,
-      "logo_path": "/6rFNo5taSC9i0Sxnl81nucQMsw9.png",
-      "name": "Warner Bros. Pictures",
-      "origin_country": "US"
-    }
-  ],
-  "production_countries": [
-    {
-      "iso_3166_1": "GB",
-      "name": "United Kingdom"
-    },
-    {
-      "iso_3166_1": "US",
-      "name": "United States of America"
-    }
-  ],
-  "release_date": "2010-07-14",
-  "revenue": 825532764,
-  "runtime": 148,
-  "spoken_languages": [
-    {
-      "iso_639_1": "en",
-      "name": "English"
-    }
-  ],
-  "status": "Released",
-  "tagline": "Your mind is the scene of the crime.",
-  "title": "Inception",
-  "video": false,
-  "vote_average": 8.2,
-  "vote_count": 16926
-};
 const testCast = [
   {name: "Brad Pitt", movieName: "Movie Name", imageUri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'},
   {name: "Other Guy", movieName: "Movie Name Of Other Guy", imageUri: 'https://shoutem.github.io/img/ui-toolkit/examples/image-11.png'},
@@ -93,12 +13,24 @@ const testCast = [
 ]
 
 class MovieDetailsScreen extends React.Component {
+  state = {}
+
+
   static navigationOptions = ({ navigation }) => {
     const { params } = navigation.state;
     return {
       title: params ? params.movieName : 'Details',
     }
   };
+
+  componentDidMount(){
+    const { params } = this.props.navigation.state;
+    axios.get(`https://movify.monus.me/title/${params.movieId}`)
+      .then(res => {
+        const movie = res.data.results;
+        this.setState(movie);
+      })
+  }
 
   renderRow(person){
     return(
@@ -122,12 +54,12 @@ class MovieDetailsScreen extends React.Component {
         <View style={{flexDirection: 'row'}}>
         <Image
           styleName="medium-square"
-          source={{uri: image_path + testMovieJson.poster_path}}
+          source={{uri: image_path + this.state.poster_path}}
         />
           <View>
-            <Text>{testMovieJson.original_title}</Text>
-            <Text>Rate: {testMovieJson.vote_average}</Text>
-            <Text>Duration: {testMovieJson.runtime} min.</Text>
+            <Text>{this.state.original_title}</Text>
+            <Text>Rate: {this.state.vote_average}</Text>
+            <Text>Duration: {this.state.runtime} min.</Text>
             <Button>
               <Text>Add to Watchlist</Text>
             </Button>
@@ -137,7 +69,7 @@ class MovieDetailsScreen extends React.Component {
           </View>
         </View>
 
-        <Text>{testMovieJson.overview}</Text>
+        <Text>{this.state.overview}</Text>
 
         <ListView
           data={testCast}
