8fb5fc028e8e5ea9b181814c4f9f37e69c06b1a2
hhakan <hhakan@sabanciuniv.edu>
2018-05-03 14:46:14 +0300

    Merge pull request #19 in SPRCOUR8/movifyapp from watchlist to master
    Squashed commit of the following:
    
    commit 678c66f237531f21463d9789b6973acc3269cbe7
    Merge: b85dfbb fe94d8e
    Author: Muvaffak Onus <onus.muvaffak@gmail.com>
    Date:   Thu May 3 13:32:45 2018 +0300
    
        merge fix
    
    commit b85dfbb63103073281666b288e5c5e526951d385
    Author: Muvaffak Onus <onus.muvaffak@gmail.com>
    Date:   Thu May 3 13:24:26 2018 +0300
    
        username connection with backend and redux
    
    commit 27f1c44cc038cc686598322ae2ac9ba40a43b845
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu May 3 12:44:53 2018 +0300
    
        -Watchlist backend connection is done
        -Watchedlist backend connecion is done
        *navigation to movie details page and add/remove button's connection should be done
    
    commit 0f29e7a2bf40ecf9ddefb257d2637b736f44cf08
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Tue May 1 15:43:10 2018 +0300
    
        -Watchlist design is done
        -add/remove button implementation is done
        *Touchable movie title should be edited
    
    commit 682073721ccbfd545d8cf776bfe76a75dafb458e
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Sun Apr 29 22:24:03 2018 +0300
    
        temp commit
    
    commit 23802caff3d5b9fd46def2b00ed715bd6d5393f1
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Wed Apr 25 19:37:49 2018 +0300
    
        watchlist gridview
    
    commit f22acfaa174c4308a2c7688d8e14231d4da284fb
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Wed Apr 25 17:20:44 2018 +0300
    
        -Edits on watchedlist page
        -Watchlist page (half-done)
    
    commit ea03d26206de4437ab631834c83bcd569a766228
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu Apr 19 20:13:58 2018 +0300
    
        Watched list half done without styling.
    
        jira SPRCOUR8-7
    
    commit fe94d8e0b5fe9e0536a130ff289c47bc3b0f0add
    Merge: b43d470 8f8138e
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu May 3 12:54:48 2018 +0300
    
        Merge branch 'watchlist' of http://ipost.sabanciuniv.edu:7990/scm/sprcour8/movifyapp into watchlist
    
    commit b43d470171872c6f0054fcd4eef84bb3692247a9
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu May 3 12:44:53 2018 +0300
    
        -Watchlist backend connection is done
        -Watchedlist backend connecion is done
        *navigation to movie details page and add/remove button's connection should be done
    
    commit ce11050789600514e984a0b757a1386bc1449282
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Tue May 1 15:43:10 2018 +0300
    
        -Watchlist design is done
        -add/remove button implementation is done
        *Touchable movie title should be edited
    
    commit 7e0e6491efa5d7675ca2591b654a8c5e529eae0b
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Sun Apr 29 22:24:03 2018 +0300
    
        temp commit
    
    commit e6ac20cf956fad7b244026f84aeb40bacd07718c
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Wed Apr 25 19:37:49 2018 +0300
    
        watchlist gridview
    
    commit 8d72c4779f938b54e17c473d8f49b2e2c12bd4a6
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Wed Apr 25 17:20:44 2018 +0300
    
        -Edits on watchedlist page
        -Watchlist page (half-done)
    
    commit 298e1b63ff0b2acee5f40e661fc91f66d7b42b14
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu Apr 19 20:13:58 2018 +0300
    
        Watched list half done without styling.
    
        jira SPRCOUR8-7
    
    commit 8f8138ef68f91f5369590390be6a842027bb1962
    Author: Hakan Harbutoglu <Hakan@Hakan-MacBook-Air-2.local>
    Date:   Thu Apr 19 20:13:58 2018 +0300
    
        Watched list half done without styling.
    
        jira SPRCOUR8-7

--- src/screens/WatchedlistScreen.js
+++ src/screens/WatchedlistScreen.js
@@ -1,18 +1,145 @@
 import React from 'react';
-import { View, Text } from '@shoutem/ui';
+import {  Text, ListView, Image, View, Button, Title, Icon } from '@shoutem/ui';
+import axios from 'axios';
+import { StackNavigator } from 'react-navigation';
+
+import { connect } from 'react-redux';
+import { userChanged } from '../actions';
+import MovieDetailsScreen from './MovieDetailsScreen';
+//const testResults = [
+//  {title: 27205, release_date:"2015", original_title: "Inception", poster_path: "http://image.tmdb.org/t/p/original/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"},
+//  {title: 27206, release_date:"2016", original_title: "Bald", poster_path: "http://image.tmdb.org/t/p/original/jUKFcsIL3WllT2AkWv4msOweJ42.jpg"},
+//  {title: 27207, release_date:"2017", original_title: "Bloodsucking Pharaohs in Pittsburgh", poster_path: "http://image.tmdb.org/t/p/original/2Hp0DHqObkKOuRweeTlJr7i9xaY.jpg"},
+//]
 
 class WatchedlistScreen extends React.Component {
+
+  state= {movieList: []}
+
   static navigationOptions = {
-    title: 'Watched',
+    title: 'Watched'
   };
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Watchedlist Screen</Text>
-      </View>
-    );
+  componentDidMount(){
+     const { params } = this.props.navigation.state;
+     axios.get(`http://localhost:3000/profile/${this.props.user.user.key}/watchedlist`)
+       .then(res => {
+         const movieList = res.data.results;
+         this.setState({movieList});
+       })
   }
-}
 
-export default WatchedlistScreen;
+    render() {
+
+      return(
+        <ListView
+          data={this.state.movieList}
+          renderRow={this.renderRow}
+        />
+      )
+
+    }
+
+      renderRow(movieList){
+        const { headerTextStyle} = styles ;
+      return (
+        <View style={styles.rowCard}>
+          <Image
+            styleName="medium-square"
+            source={{uri: movieList.poster_path}}
+          />
+          <View style={{ flex: 1, marginHorizontal: 8}}>
+            <Title
+            style={{marginVertical: 4}}>{movieList.original_title}
+            </Title>
+            <Text style={headerTextStyle}>{movieList.releaseDate}</Text>
+            <View style={{flexDirection: 'row', alignSelf: 'flex-end', marginVertical: 5 }}>
+
+              <Button style={styles.smallButton}><Icon name="checkbox-on" /></Button>
+            </View>
+          </View>
+        </View>
+      );
+  }
+  }
+
+  const styles = {
+    rowCard: {
+      flexDirection: 'row',
+      marginVertical: 8,
+      backgroundColor: 'white',
+      alignItems: 'center'
+    },
+    headerTextStyle: {
+      fontSize: 18
+    },
+    smallButton: {
+      paddingHorizontal: 5
+    }
+  }
+
+
+
+
+
+
+
+    render() {
+
+      return(
+        <ListView
+          data={this.state.movieList}
+          renderRow={this.renderRow}
+        />
+      )
+
+    }
+
+      renderRow(movieList){
+        const { headerTextStyle} = styles ;
+      return (
+        <View style={styles.rowCard}>
+          <Image
+            styleName="medium-square"
+            source={{uri: movieList.poster_path}}
+          />
+          <View style={{ flex: 1, marginHorizontal: 8}}>
+            <Title
+            style={{marginVertical: 4}}>{movieList.original_title}
+            </Title>
+            <Text style={headerTextStyle}>{movieList.releaseDate}</Text>
+            <View style={{flexDirection: 'row', alignSelf: 'flex-end', marginVertical: 5 }}>
+
+              <Button style={styles.smallButton}><Icon name="checkbox-on" /></Button>
+            </View>
+          </View>
+        </View>
+      );
+  }
+  }
+
+  const styles = {
+    rowCard: {
+      flexDirection: 'row',
+      marginVertical: 8,
+      backgroundColor: 'white',
+      alignItems: 'center'
+    },
+    headerTextStyle: {
+      fontSize: 18
+    },
+    smallButton: {
+      paddingHorizontal: 5
+    }
+  }
+const mapStateToProps = ({ allReducers }) => {
+  const { user } = allReducers;
+  return { user };
+};
+
+const WatchedlistStack = StackNavigator({
+  Home: { screen: connect(mapStateToProps, { userChanged })(WatchedlistScreen)},
+  MovieDetails: { screen: MovieDetailsScreen },
+});
+
+export default WatchedlistStack;
--- src/screens/WatchlistScreen.js
+++ src/screens/WatchlistScreen.js
@@ -1,30 +1,86 @@
 import React from 'react';
-import { View, Text, Button } from '@shoutem/ui';
+import {  Text, ListView, Image, View, Button, Title, Icon } from '@shoutem/ui';
 import { StackNavigator } from 'react-navigation';
 import { connect } from 'react-redux';
 import { userChanged } from '../actions';
-
+import axios from 'axios';
 import MovieDetailsScreen from './MovieDetailsScreen';
 
+
+//const testResults = [
+//  {title: 27205, release_date:"2015", original_title: "Inception", poster_path: "http://image.tmdb.org/t/p/original/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg"},
+//  {title: 27206, release_date:"2016", original_title: "Bald", poster_path: "http://image.tmdb.org/t/p/original/jUKFcsIL3WllT2AkWv4msOweJ42.jpg"},
+//  {title: 27207, release_date:"2017", original_title: "Bloodsucking Pharaohs in Pittsburgh", poster_path: "http://image.tmdb.org/t/p/original/2Hp0DHqObkKOuRweeTlJr7i9xaY.jpg"},
+//]
+
 class WatchlistScreen extends React.Component {
+
+  state= {movieList: []}
+
   static navigationOptions = {
     title: 'Watchlist'
   };
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-        styleName="secondary" style={{ marginTop: 20 }}
-        onPress={() => this.props.navigation
-          .navigate('MovieDetails', {movieName: "Inception", movieId: "27205"})}
-        >
-        <Text>Inception</Text>
-        </Button>
-      </View>
-    );
+  componentDidMount(){
+     const { params } = this.props.navigation.state;
+     axios.get(`http://localhost:3000/profile/${this.props.user.user.key}/watchlist`)
+       .then(res => {
+         const movieList = res.data.results;
+         this.setState({movieList});
+       })
   }
-}
+
+    render() {
+
+      return(
+        <ListView
+          data={this.state.movieList}
+          renderRow={this.renderRow}
+        />
+      )
+
+    }
+
+      renderRow(movieList){
+        const { headerTextStyle} = styles ;
+      return (
+        <View style={styles.rowCard}>
+          <Image
+            styleName="medium-square"
+            source={{uri: movieList.poster_path}}
+          />
+          <View style={{ flex: 1, marginHorizontal: 8}}>
+            <Title
+            onPress={() => this.props.navigation.navigate('MovieDetailsScreen')}
+            style={{marginVertical: 4}}>{movieList.original_title}
+            </Title>
+            <Text style={headerTextStyle}>{movieList.releaseDate}</Text>
+            <View style={{flexDirection: 'row', alignSelf: 'flex-end', marginVertical: 5 }}>
+
+              <Button style={styles.smallButton}><Icon name="checkbox-on" /></Button>
+            </View>
+          </View>
+        </View>
+      );
+  }
+  }
+
+  const styles = {
+    rowCard: {
+      flexDirection: 'row',
+      marginVertical: 8,
+      backgroundColor: 'white',
+      alignItems: 'center'
+    },
+    headerTextStyle: {
+      fontSize: 18
+    },
+    smallButton: {
+      paddingHorizontal: 5
+    }
+  }
+
+
 const mapStateToProps = ({ allReducers }) => {
   const { user } = allReducers;
   return { user };
