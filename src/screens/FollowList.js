91935b9f41e5965ab741d64bb3253126fb81793b
Bora Ikizoglu <boraikizoglu@gmail.com>
2018-05-06 06:15:11 +0300

    FollowList is added to navigation. Get functions are written

---
+++ src/screens/FollowList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text } from '@shoutem/ui';
+
+export default class FollowList extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    Profile search page
+                </Text>
+            </View>
+        );
+    }
+}
